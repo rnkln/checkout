ce3e35011cef3bb5e34bb92fea248912fc53b5dd
Rune Klein <runeklein@outlook.com>
2024-09-25 13:14:57 +0200

    🐛 please

--- scripts/init.js
+++ scripts/init.js
@@ -7,7 +7,7 @@ const directory = p.dirname(filename)
 
 const env = {
 	NEXT_TELEMETRY_DISABLED: 1,
-	NEXT_PUBLIC_PAYMENTS_API_ENDPOINT: 'http://localhost:3001/api'
+	NEXT_PUBLIC_API_ENDPOINT: 'http://localhost:3001/api'
 }
 
 const envFile = p.resolve(directory, '..', '.env.local')
