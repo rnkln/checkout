6cfc5ced137c2fbb279095178106d91a01bc1014
KleinMann <47398070+rnkln@users.noreply.github.com>
2024-10-29 12:13:57 +0100

    ✨ Implement cors in mock, and prepare for vercel deployment (#4)

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,7 @@
 export async function register() {
 	if (
-		process.env.NODE_ENV === 'development' &&
-		process.env.NEXT_RUNTIME === 'nodejs'
+		process.env.NEXT_RUNTIME === 'nodejs' &&
+		process.env.NEXT_PUBLIC_API_MOCK === 'true'
 	) {
 		await import('./mock/src/server')
 	}
--- mock/src/server.ts
+++ mock/src/server.ts
@@ -1,23 +1,31 @@
-import { createServer } from '@mswjs/http-middleware'
 import chalk from 'chalk'
-
-import { createDelay } from './helpers/delay'
+import express from 'express'
+import { createMiddleware } from '@mswjs/http-middleware'
+import { createCorsMiddleware } from './helpers/cors'
+import { createDelayMiddleware } from './helpers/delay'
 import { createVaultService } from './services/vault'
 import { createPaymentService } from './services/payment'
 import { createChallengeService } from './services/challenge'
 
+const app = express()
+const url = new URL(process.env.NEXT_PUBLIC_API_ENDPOINT ?? '')
 const port = 3001
-const hostname = `http://localhost:${port}`
-const server = createServer(
-	...[
-		...createDelay('real'),
-		...createVaultService('/api/vault'),
-		...createPaymentService('/api/payments'),
-		...createChallengeService('/api/challenge')
-	]
+
+app.use(createCorsMiddleware())
+app.use(createDelayMiddleware(200))
+app.use(
+	createMiddleware(
+		...[
+			...createVaultService(`${url.pathname}/vault`),
+			...createPaymentService(`${url.pathname}/payments`),
+			...createChallengeService(`${url.pathname}/challenge`)
+		]
+	)
 )
 
-server.listen(port, () => {
+app.listen(url.port, () => {
 	// eslint-disable-next-line no-console
-	console.log(` ${chalk.green('✓')} Server mock is running at ${hostname}`)
+	console.log(
+		` ${chalk.green('✓')} Server mock is running at http://localhost:${port}`
+	)
 })
